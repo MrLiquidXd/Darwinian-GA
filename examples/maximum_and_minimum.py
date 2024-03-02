61ed3766ca6027fd57f947f822eafce0e0ba3d04
Ars <161674051+MrLiquidXd@users.noreply.github.com>
2024-03-02 16:46:43 +0700

    Update maximum_and_minimum.py

--- examples/maximum_and_minimum.py
+++ examples/maximum_and_minimum.py
@@ -1,9 +1,9 @@
 # импорт библиотек
-from darwinian import darwinian_GA
+from darwinian import Darwinian_GA
 import random
 
 # Во первых мы создадим экземпляр maxGA
-maxGA = darwinian_GA()
+maxGA = Darwinian_GA()
 
 # Функция приспособленности
 def fitness(individual):
@@ -13,8 +13,8 @@ def fitness(individual):
 maxGA.setup(10, 10, 0.01, 0.8, 100, fitness, lambda: random.randint(0, 1))
 
 # запускаем
-max_individual = maxGA.bestparents(MaxOrMin=True)
-min_individual = maxGA.bestparents(MaxOrMin=False)
+max_individual = maxGA.constructor(MaxOrMin=True)
+min_individual = maxGA.constructor(MaxOrMin=False)
 
 print(max_individual[0])
 print(min_individual[0])
